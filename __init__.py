d80d5c12aa0e71b3e5e3e4acc3b0033fe9987415
Andrew J Plaza <aplaza@sandiego.edu>
2021-03-16 20:11:26 -0700

    Restructured

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-from commandParser import purchaseTicket
+from src.commandParser import purchaseTicket
 
 class Purchaseticket(MycroftSkill):
     def __init__(self):
