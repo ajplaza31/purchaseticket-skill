1698c03f1b2816b7729b388eaa4e82ce0bc5f5d0
Andrew J Plaza <aplaza@sandiego.edu>
2021-03-21 20:47:31 -0700

    Copied command parser code into init

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-from src.commandParser import purchaseTicket
+import sqlite3
 
 class Purchaseticket(MycroftSkill):
     def __init__(self):
@@ -7,8 +7,59 @@ class Purchaseticket(MycroftSkill):
 
     @intent_file_handler('purchaseticket.intent')
     def handle_purchaseticket(self, message):
-        purchaseTicket()
-        self.speak_dialog('purchaseticket')
+        
+        conn = sqlite3.connect("cubic.sql")
+        cur = conn.cursor()
+
+        cur.execute("SELECT * FROM PassData")
+        rows = cur.fetchall()
+        
+        i=1
+        
+        self.speak('Here are the available tickets.')
+        
+        for row in rows:
+            cur.execute("SELECT * FROM TransitLine WHERE LineID = ?", (row[3],))
+            idrow = cur.fetchone()
+            self.speak('Ticket {} starts at {}, ends at {}, has an E.T.A of {}, and costs {}.'.format(i, row[4], row[5], idrow[3], row[6]))
+            i += 1
+
+        n = int(input("Which ticket would you like to select? \n"))
+        m = n
+        n = n - 1
+
+        cur.execute("SELECT * FROM PassData LIMIT 1 OFFSET ?", (n,))
+        ticket = cur.fetchone()
+
+        cur.execute("SELECT * FROM TransitLine WHERE LineID = ?", (ticket[3],))
+
+        idrow = cur.fetchone()
+
+        self.speak("You are about to purchase the following ticket: \n")
+        self.speak(" %d. Start: %-*s  End: %-*s  ETA: %-*s  Cost: $%s\n" % (m, 20, ticket[4], 20, ticket[5], 20, idrow[3], ticket[6]))
+        answer = input("Would you like to proceed? (y/n)\n ")
+    
+        cardNo = 0
+        if (answer == "y"):
+        cardNo = (int)(input("Please enter your credit card number: \n"))
+
+        cur.execute("SELECT * FROM Customer LIMIT 1 OFFSET ?", (n,))
+        savedNo = cur.fetchall()
+        choice = ""
+
+        if (len(savedNo) == 0):
+            choice = input("Would you like to save your payment info? (y/n)\n")
+        else:
+            self.speak("Thank you for purchasing! \n")
+
+        if (choice == "y"):
+            name = input("What is your name? \n")
+            customerNo = random.randint(11,10000)
+            cur.execute("INSERT INTO Customer(CustomerID, SavedPaymentInfo, Name) VALUES(?, ?, ?)", (customerNo, cardNo, name))
+        elif (choice == "n"):
+            self.speak("Thank you for purhcasing!\n")
+
+        #self.speak_dialog('purchaseticket')
 
 
 def create_skill():
