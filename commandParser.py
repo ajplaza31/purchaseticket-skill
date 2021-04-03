6e078e32ddfa8c984840722789af616c268a6776
Andrew J Plaza <aplaza@sandiego.edu>
2021-04-03 16:47:58 -0700

    updated commandParser module

--- commandParser.py
+++ commandParser.py
@@ -64,44 +64,46 @@ def getRoute():
   return None
 
 def purchaseTicket():
-    cur = conn.cursor()
+  cur = conn.cursor()
 
-    listTickets()
+  listTickets()
 
-    n = int(input("Which ticket would you like to select? \n"))
-    m = n
-    n = n - 1
+  n = int(input("Which ticket would you like to select? \n"))
+  m = n
+  n = n - 1
 
-    cur.execute("SELECT * FROM PassData LIMIT 1 OFFSET ?", (n,))
-    ticket = cur.fetchone()
+  cur.execute("SELECT * FROM PassData LIMIT 1 OFFSET ?", (n,))
+  ticket = cur.fetchone()
 
-    cur.execute("SELECT * FROM TransitLine WHERE LineID = ?", (ticket[3],))
+  cur.execute("SELECT * FROM TransitLine WHERE LineID = ?", (ticket[3],))
 
-    idrow = cur.fetchone()
+  idrow = cur.fetchone()
 
-    print("You are about to purchase the following ticket: \n")
-    print(" %d. Start: %-*s  End: %-*s  ETA: %-*s  Cost: $%s\n" % (m, 20, ticket[4], 20, ticket[5], 20, idrow[3], ticket[6]))
-    answer = input("Would you like to proceed? (y/n)\n ")
+  print("You are about to purchase the following ticket: \n")
+  print(" %d. Start: %-*s  End: %-*s  ETA: %-*s  Cost: $%s\n" % (m, 20, ticket[4], 20, ticket[5], 20, idrow[3], ticket[6]))
+  answer = input("Would you like to proceed? (y/n)\n ")
     
-    cardNo = 0
-    if (answer == "y"):
-      cardNo = (int)(input("Please enter your credit card number: \n"))
+  cardNo = 0
+  if (answer == "y"):
+  cardNo = (int)(input("Please enter your credit card number: \n"))
 
-    cur.execute("SELECT * FROM Customer LIMIT 1 OFFSET ?", (n,))
-    savedNo = cur.fetchall()
-    choice = ""
+  cur.execute("SELECT * FROM Customer WHERE SavedPaymentInfo = ?", (cardNo,))
+  savedNo = cur.fetchone()
+  choice = ""
 
-    if (len(savedNo) == 0):
-      choice = input("Would you like to save your payment info? (y/n)\n")
-    else:
-      print("Thank you for purchasing! \n")
+  if (savedNo != None):
+    print("Thank you for purchasing! \n")
+  elif (savedNo == None):
+    choice = input("Would you like to save your payment info? (y/n)\n")
 
-    if (choice == "y"):
-      name = input("What is your name? \n")
-      customerNo = random.randint(11,10000)
-      cur.execute("INSERT INTO Customer(CustomerID, SavedPaymentInfo, Name) VALUES(?, ?, ?)", (customerNo, cardNo, name))
-    elif (choice == "n"):
-      print("Thank you for purhcasing!\n")
+  if (choice == "y"):
+    name = input("What is your name? \n")
+    customerNo = random.randint(11,10000)
+    cur.execute("INSERT INTO Customer(CustomerID, SavedPaymentInfo, Name) VALUES(?, ?, ?)", (customerNo, cardNo, name))
+    conn.commit()
+    print("Thank you for purchasing!\n")
+  elif (choice == "n"):
+    print("Thank you for purchasing!\n")
 
       
       
