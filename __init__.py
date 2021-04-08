c28b084f8d59db524c786e8104a64feeab236188
Andrew J Plaza <aplaza@sandiego.edu>
2021-04-08 14:19:13 -0700

    moved some stuff around again

--- __init__.py
+++ __init__.py
@@ -4,12 +4,11 @@ import random
 
 class Purchaseticket(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
-        conn = sqlite3.connect("cubic.sql")   
+        MycroftSkill.__init__(self) 
 
     @intent_file_handler('purchaseticket.intent')
     def handle_purchaseticket(self, message):
-
+        conn = sqlite3.connect("cubic.sql") 
         cur = conn.cursor()
     
         cur.execute("SELECT * FROM PassData")
