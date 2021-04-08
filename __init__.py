16b9e2a6f9b3379c94ba76bc82960f85657648ef
Andrew J Plaza <aplaza@sandiego.edu>
2021-04-08 13:28:48 -0700

    changed responses back to ints

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@ class Purchaseticket(MycroftSkill):
             self.speak('Ticket {} starts at {}, ends at {}, has an E.T.A of {}, and costs {}.'.format(i, row[4], row[5], idrow[3], row[6]))
             i += 1
 
-        n = self.get_response('Which ticket would you like to select?')
+        n = (int)self.get_response('Which ticket would you like to select?')
         m = n
         n = n - 1
 
@@ -42,14 +42,14 @@ class Purchaseticket(MycroftSkill):
     
         cardNo = 0
         if (answer == "yes"):
-            cardNo = self.get_response('Please enter your credit card number: ')
+            cardNo = (int)self.get_response('Please enter your credit card number: ')
 
         cur.execute("SELECT * FROM Customer WHERE SavedPaymentInfo = ?", (cardNo,))
         savedNo = cur.fetchone()
         choice = ""
 
         if (savedNo != None):
-            choice = self.ask_yesno("Would you like to save your payment info? (y/n)\n")
+            choice = self.ask_yesno("Would you like to save your payment info? (y/n)")
         elif (savedNo == None):
             self.speak("Thank you for purchasing!")
 
